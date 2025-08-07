cf725f9e95779542175a59f5d55c5941c5f4a0b9
YonierR <yonierruiz1234@gmail.com>
2025-08-07 02:52:24 -0500

    C

--- Abstraccion.py
+++ Abstraccion.py
@@ -6,21 +6,3 @@ class Forma(ABC):
     def calcular_area(self):
         pass
 
-
-class Circulo(Forma):
-    def __init__(self, radio):
-        self.radio = radio
-    
-    
-    def calcular_area(self):
-        return 3.14159 * (self.radio ** 2)
-
-
-try:
-    forma = Forma()
-except TypeError as e:
-    print(f"Error al instanciar clase abstracta: {e}")
-
-
-circulo = Circulo(5)
-print(f"Área del círculo con radio 5: {circulo.calcular_area():.2f}")
