6defbbab485ad1516920df69145d9b26baefda26
YonierR <yonierruiz1234@gmail.com>
2025-08-07 03:09:21 -0500

    C

--- Abstraccion.py
+++ Abstraccion.py
@@ -6,3 +6,13 @@ class Forma(ABC):
     def calcular_area(self):
         pass
 
+class Circulo(Forma):
+    def __init__(self, radio):
+        self.radio = radio
+    
+    
+    def calcular_area(self):
+        return 3.14159 * (self.radio**2)
+    
+circulo=Circulo(2)
+print(f"area de radio 2: {circulo.calcular_area()}")
