d18355a6f5b13293edb3dbb849d620f9e91785c6
YonierR <yonierruiz1234@gmail.com>
2025-08-06 23:12:18 -0500

    C

--- Libro.py
+++ Libro.py
@@ -1,16 +1,16 @@
 #Taller  de programacion 10
 
-class libro:
+class Libro:
     def __init__(self,titulo,autor,año):
         self.titulo=titulo
         self.autor=autor
         self.año=año
+        
+    def mostrar_info(self):
+        print(f"Del año:{self.año}")
 
-def mostrar_info(self):
-    print(f"Del año:{self.año}")
+mi_libro=Libro ("Orgullo y prejuicio","Jane Austen", 1813)
 
-mi_libro=libro ("Orgullo y prejuicio","jane austen", 1813)
-
-print(f"Mio libro es {mi_libro.titulo} de {mi_libro.autor}")
+print(f"Mi libro es {mi_libro.titulo} de {mi_libro.autor}")
 
 mi_libro.mostrar_info()
