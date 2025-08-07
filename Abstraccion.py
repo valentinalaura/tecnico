11b3c76a40ad9baac39d55d265077033e3347291
YonierR <yonierruiz1234@gmail.com>
2025-08-07 03:23:27 -0500

    C

--- Abstraccion.py
+++ Abstraccion.py
@@ -13,6 +13,8 @@ class Circulo(Forma):
     
     def calcular_area(self):
         return 3.14159 * (self.radio**2)
-    
+
+#forma=Forma()
+
 circulo=Circulo(2)
 print(f"area de radio 2: {circulo.calcular_area()}")
--- Capsula.py
+++ Capsula.py
@@ -1,5 +1,13 @@
 #Taller de programacion 12
 
 class Producto:
-    def __call__(self,Privado):
-        self.privado=Privado
+    def __init__(self,precio):
+        self.__precio=precio
+    
+    def get_precio(self):
+        return self.__precio
+    
+Producto=Producto(200)
+
+#print(Producto.__precio)
+print(f"Precio obtenido es:{Producto.get_precio()}")
