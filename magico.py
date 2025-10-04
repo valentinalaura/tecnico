a40d5c160cb08ad9227f59865804d2eb30b85636
YonierR <yonierruiz1234@gmail.com>
2025-10-04 17:23:47 -0500

    C

--- magico.py
+++ magico.py
@@ -1,5 +1,16 @@
 #Taller de programacion 15
 
+
+class Persona:
+    def __init__(self,nombre,edad):
+        self.nombre=nombre
+        self.edad=edad
+
+    def __str__(self):
+        return f"Persona(nombre:{self.nombre}, edad:{self.edad})"
+
+P= Persona("antonio", 15)
+
 class Estudiante:
     def __init__(self,nombre,edad):
         self.nombre=nombre
@@ -9,4 +20,4 @@ class Estudiante:
         return f"Estudiante(nombre:{self.nombre}, edad:{self.edad})"
     
 E= Estudiante("luis", 17)
-print(E)
+print(E,P)
